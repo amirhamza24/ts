19e3d4abec44cfcb6316639eb43876ef83c4f795
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2024-04-04 12:41:03 +0600

    update

--- example-ts/unionDataType.ts
+++ example-ts/unionDataType.ts
@@ -4,7 +4,7 @@ userId = 123;  // no error
 userId = "Hello World";  // no error
 // userId = true; // error, cause this is 'boolean' data type and this data type is not define.
 
-console.log("user: ", userId);  // output: "Hello World"
+console.log("user: ", userId);  // output: "Hello World" 
 
 function userInfo(userId: string | number) {
     console.log("function user: ", userId);
